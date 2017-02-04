3e8be5dcbac718a47f345e88f13a12aafb4ed378
Mohanjit Gill <mohanjitgill4@gmail.com>
2017-02-04 18:11:05 -0500

    Update game.js

--- Project/game.js
+++ Project/game.js
@@ -134,7 +134,7 @@ function exitGame()
 function enterHelp()
 {
 	console.log("Entering help state.");
-	_stage.style.backgroundColor = "white";
+	document.body.style.backgroundImage = "https://github.com/GameGibbons/Prison-Escape-Game/tree/master/Project/img('ControlsPage.jpg')";
 	activeBtns = [ buttons[2] ];
 }
 
